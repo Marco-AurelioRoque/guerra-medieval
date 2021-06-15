470653aaa5ccf3b01775ebc3f877ec1c0476bfa5
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-06-14 21:34:26 -0300

    update

--- config/script.js
+++ config/script.js
@@ -1,10 +1,40 @@
 var mainPrincipal = document.querySelector("main")
 var referencias = document.querySelector(".referencia-wrapper")
-var casteloSR = document.querySelector(".castle-wrapper").display = "block"
+var casteloSR = document.querySelector(".castle-wrapper")
+
+var aba1 = document.querySelector(".aba-open")
+var aba2 = document.querySelector(".aba-open2")
+var aba3 = document.querySelector(".aba-open3")
+
+//Abas
+function markAba() {
+    aba1.style.display = "block"
+    aba2.style.display = "none"
+    aba3.style.display = "none"
+}
+
+function markAba2() {
+    aba1.style.display = "none"
+    aba2.style.display = "block"
+    aba3.style.display = "none"
+}
+
+function markAba3() {
+    aba1.style.display = "none"
+    aba2.style.display = "none"
+    aba3.style.display = "block"
+}
+
+function contentPrincipal() {
+    referencias.style.display = "none"
+    mainPrincipal.style.display = "block"
+    casteloSR.style.display = "none"
+}
 
 function refer() {
     referencias.style.display = "block"
     mainPrincipal.style.display = "none"
+    casteloSR.style.display = "none"
 }
 
 function castle() {
